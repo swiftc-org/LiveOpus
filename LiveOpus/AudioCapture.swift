729148bfc583ba89e8db5a7eb0d2117fd9c8fc86
swiftc-org <a@swiftc.org>
2016-06-28 21:55:33 +0800

    add libopus

---
+++ LiveOpus/AudioCapture.swift
@@ -0,0 +1,21 @@
+//
+//  AudioCapture.swift
+//  LiveOpus
+//
+//  Created by 成杰 on 16/6/28.
+//  Copyright © 2016年 swiftc.org. All rights reserved.
+//
+
+import UIKit
+import CoreAudio
+import AudioToolbox
+
+class AudioCapture: NSObject {
+    
+    private var queue: AudioQueueRef!
+    
+    override init() {
+        super.init()
+        
+    }
+}
---
+++ LiveOpus/bridge.h
@@ -0,0 +1,16 @@
+//
+//  bridge.h
+//  LiveOpus
+//
+//  Created by 成杰 on 16/6/28.
+//  Copyright © 2016年 swiftc.org. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+#include "rtmp.h"
+
+#include "opus.h"
+
+#endif /* bridge_h */
